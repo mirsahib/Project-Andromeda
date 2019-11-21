ff5889989b514142629d2fbd99f2cfaf9bc610c7
mirsahib <mirsahib24@gmail.com>
2019-11-21 18:24:07 +0600

    test branch

--- Code/2019-IEEE_Access/d_tree.py
+++ Code/2019-IEEE_Access/d_tree.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
 
 
 path = r"C:\Users\Mir Sahib\Desktop/Project-Andromeda/Dataset/demo/combined_csv.csv"
@@ -15,10 +18,12 @@ X = df.values[:,0:col-1]
 Y = df.values[:,-1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20)
+model = tree.DecisionTreeClassifier()
+model.fit(X_train, y_train)
 
-svclassifier = SVC(kernel='linear')
-svclassifier.fit(X_train, y_train)
-y_pred = svclassifier.predict(X_test)
+y_predict = model.predict(X_test)
 
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
+print(accuracy_score(y_test, y_predict))
+
+print(confusion_matrix(y_test, y_predict))
+    
