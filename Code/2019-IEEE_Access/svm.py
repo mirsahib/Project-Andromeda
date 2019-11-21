705c894505de63fc6de7f5ad4dd1eebc30743186
mirsahib <mirsahib24@gmail.com>
2019-11-21 19:22:12 +0600

    svm done with 2 level sperately

--- Code/2019-IEEE_Access/svm.py
+++ Code/2019-IEEE_Access/svm.py
@@ -13,7 +13,7 @@ from sklearn import datasets
 #linux
 #path = r"/home/mirsahib/Desktop/Project-Andromeda/Dataset/1st_Level_Feature_Extracted/mega.csv"
 
-path = r"C:\Users\Mir Sahib\Desktop\Project-Andromeda\Dataset\1st_Level_Feature_Extracted\combined_csv.csv"
+path = r"C:\Users\Mir Sahib\Desktop\Project-Andromeda\Dataset\2nd_Level_Feature_Extracted\combined_csv.csv"
 
 df = pd.read_csv(path)
 col = df.shape[1]
@@ -42,4 +42,39 @@ for train_index, test_index in kfold.split(df):
 
 print("Average Accuracy: "+str(sum(acc_score)/10))
 
+kfold = KFold(fold, True, 1)
+acc_score = []
+count=1
+for train_index, test_index in kfold.split(df):
+  X_train, X_test, y_train, y_test = X[train_index], X[test_index], Y[train_index], Y[test_index]
+  #SVM
+  X_train = scaler.fit_transform(X_train)
+  X_test = scaler.fit_transform(X_test)
+  svm_model_poly = SVC(kernel = 'poly',gamma='auto').fit(X_train, y_train) 
+  svm_predictions = svm_model_poly.predict(X_test) 
+  accuracy = svm_model_linear.score(X_test, y_test)
+  acc_score.append(accuracy*100)
+  print('Accuracy in fold '+str(count)+' is '+str(accuracy*100))
+  count = count+1
+
+print("Average Accuracy: "+str(sum(acc_score)/10))
+
+"""# SVM RBF"""
+
+kfold = KFold(fold, True, 1)
+acc_score = []
+count=1
+for train_index, test_index in kfold.split(df):
+  X_train, X_test, y_train, y_test = X[train_index], X[test_index], Y[train_index], Y[test_index]
+  #SVM
+  X_train = scaler.fit_transform(X_train)
+  X_test = scaler.fit_transform(X_test)
+  svm_model_rbf = SVC(kernel = 'rbf',gamma='auto').fit(X_train, y_train) 
+  svm_predictions = svm_model_rbf.predict(X_test) 
+  accuracy = svm_model_linear.score(X_test, y_test)
+  acc_score.append(accuracy*100)
+  print('Accuracy in fold '+str(count)+' is '+str(accuracy*100))
+  count = count+1
+
+print("Average Accuracy: "+str(sum(acc_score)/10))
 
--- Code/Util/merge_file.py
+++ Code/Util/merge_file.py
@@ -10,7 +10,7 @@ import os
 import glob
 import pandas as pd
 
-mydir = os.chdir(r"C:\Users\Mir Sahib\Desktop\Project-Andromeda\Dataset\demo")
+mydir = os.chdir(r"C:\Users\Mir Sahib\Desktop\Project-Andromeda\Dataset\1st_Level_Feature_Extracted")
 extension = "csv"
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 #combine all files in the list
